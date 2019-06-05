992f0845519bf76f0886e8c8f756d395032a1374
Carlos Fábio R Conceição <carlos.conceicao@hdi.com.br>
2019-06-05 18:13:10 -0300

    Esboco inicial do projeto

---
+++ getPolicy/src/main/java/com/hdi/integration/getPolicy/GetPolicyApplication.java
@@ -0,0 +1,13 @@
+package com.hdi.integration.getPolicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetPolicyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetPolicyApplication.class, args);
+	}
+
+}
