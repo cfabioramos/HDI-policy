9f96b67ba79ead6f9cab3fbaed56e028ab557057
Carlos Fábio R Conceição <carlos.conceicao@hdi.com.br>
2019-06-10 10:35:04 -0300

    Mudanca no nome do modulo para: ensurancePolicyDetails

--- ensurancePolicyDetails/src/main/java/com/hdi/integration/ensurancePolicyDetails/EnsurancePolicyDetailsApplication.java
+++ ensurancePolicyDetails/src/main/java/com/hdi/integration/ensurancePolicyDetails/EnsurancePolicyDetailsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GetPolicyApplication {
+public class EnsurancePolicyDetailsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GetPolicyApplication.class, args);
+		SpringApplication.run(EnsurancePolicyDetailsApplication.class, args);
 	}
 
 }
--- ensurancePolicyDetails/src/test/java/com/hdi/integration/ensurancePolicyDetails/EnsurancePolicyDetailsApplicationTests.java
+++ ensurancePolicyDetails/src/test/java/com/hdi/integration/ensurancePolicyDetails/EnsurancePolicyDetailsApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GetPolicyApplicationTests {
+public class EnsurancePolicyDetailsApplicationTests {
 
 	@Test
 	public void contextLoads() {
